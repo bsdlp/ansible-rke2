043a0ab4a001cc6002de89c66a05b901a8d2902e
Jon Chen <bsd@voltaire.sh>
2023-07-05 00:54:25 -0700

    init

---
+++ roles/vm_host/files/terraform/libvirt-resources/libvirt-resources.tf
@@ -0,0 +1,37 @@
+variable "domain" { default = "k8s.lab" }
+variable "network_cidr" {
+  type    = list(any)
+  default = ["192.168.100.0/24"]
+}
+variable "cluster_name" { default = "k8s" }
+variable "libvirt_pool_path" { default = "/var/lib/libvirt/images" }
+variable "bridge_name" { default = "br0" }
+variable "qemu_url" { default = "qemu:///system" }
+
+provider "libvirt" {
+  uri = "${var.qemu_url}"
+}
+
+resource "libvirt_pool" "cluster" {
+  name = var.cluster_name
+  type = "dir"
+  path = "${var.libvirt_pool_path}/${var.cluster_name}"
+}
+
+resource "libvirt_network" "kube_network" {
+  autostart = true
+  name      = var.cluster_name
+  mode      = "bridge"
+  bridge    = var.bridge_name
+}
+
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    libvirt = {
+      source  = "registry.terraform.io/dmacvicar/libvirt"
+      version = "0.7.1"
+    }
+  }
+}
+
